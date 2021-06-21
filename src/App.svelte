d1f220d13891467bc8c665a5eb02a6db52905e35
zeeshanok <66509181+zeeshanok@users.noreply.github.com>
2021-06-21 20:19:24 +0400

    update to webpack v5

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,14 @@
     let name: string = "world";
 </script>
 
+<main>
+    <h1>Hello {name}!</h1>
+    <input type="text" bind:value={name}/>
+</main>
+
 <style>
     main {
         text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
+        font-family: "Segoe UI", sans-serif;
     }
 </style>
-
-<main>
-    <h1>Hello {name}!</h1>
-    <input type="text" bind:value={name} />
-</main>
--- webpack.config.js
+++ webpack.config.js
@@ -3,62 +3,99 @@ const path = require('path');
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
+const port = 8080;
 
 module.exports = {
-    entry: {
-        bundle: ['./src/main.ts']
+  entry: {
+    bundle: ['./src/main.ts']
+  },
+  resolve: {
+    alias: {
+      "svelte": path.resolve('node_modules', 'svelte')
     },
-    resolve: {
-        alias: {
-            svelte: path.resolve('node_modules', 'svelte')
-        },
-        extensions: ['.mjs', '.js', '.ts', '.svelte'],
-        mainFields: ['svelte', 'browser', 'module', 'main']
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].js',
-        chunkFilename: '[name].[id].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.svelte$/,
-                use: {
-                    loader: 'svelte-loader',
-                    options: {
-                        hotReload: true,
-                        preprocess: require('svelte-preprocess')({})
-                    }
-                }
+    extensions: ['.mjs', '.js', '.ts', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            preprocess: require('svelte-preprocess')(),
+            compilerOptions: {
+              dev: !prod
             },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    /**
-                     * MiniCssExtractPlugin doesn't support HMR.
-                     * For developing, use 'style-loader' instead.
-                     * */
-                    prod ? MiniCssExtractPlugin.loader : 'style-loader',
-                    'css-loader'
-                ]
-            }
+            emitCss: true,
+            hotReload: !prod
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
         ]
-    },
-    mode,
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
-    ],
-    devtool: prod ? false : 'source-map'
+      },
+      {
+        // required to prevent errors from Svelte on Webpack 5+
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false
+        }
+      }
+      // {
+      //     test: /\.(html|svelte)$/,
+      //     use: 'svelte-loader'
+      // },
+      // {
+      //     // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
+      //     test: /node_modules\/svelte\/.*\.mjs$/,
+      //     resolve: {
+      //         fullySpecified: false
+      //     }
+      // },
+      // {
+      //     test: /\.ts$/,
+      //     exclude: /node_modules/,
+      //     use: [
+      //         {
+      //             loader: 'ts-loader'
+      //         }
+      //     ]
+      // },
+      // {
+      //     test: /\.css$/,
+      //     use: [
+      //         /**
+      //          * MiniCssExtractPlugin doesn't support HMR.
+      //          * For developing, use 'style-loader' instead.
+      //          * */
+      //         // prod ? MiniCssExtractPlugin.loader : 'style-loader',
+      //         // 'css-loader'
+      //         'css-loader'
+      //     ]
+      // }
+    ]
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: '[name].css'
+    })
+  ],
+  devtool: prod ? false : 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: port,
+    host: 'localhost',
+    historyApiFallback: true,
+    hot: true,
+  },
 };
